b84daeae63e0b392b2bf6136dc8847e7dfc0709e
Gorka Olalde <gorka.olalde@gmail.com>
2014-06-11 00:12:36 +0200

    Initial Commit

---
+++ eskola kudeaketa/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  eskola kudeaketa
+//
+//  Created by Gorka Olalde on 11/06/14.
+//  Copyright (c) 2014 Gorka Olalde. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
